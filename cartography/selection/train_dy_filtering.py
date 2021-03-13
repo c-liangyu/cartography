06898b917952ee1958ea75f3abf7e6f8f9e6e9a2
Alisa Liu <alisa.46@gmail.com>
2021-03-12 21:47:41 -0800

    add mean variability and final confidence metrics, mixed selection with ambiguous and easy-to-learn examples, generalize for training dynamics on dev data (e.g. unlabeled data for active learning)

--- cartography/selection/train_dy_filtering.py
+++ cartography/selection/train_dy_filtering.py
@@ -16,7 +16,7 @@ import os
 import pandas as pd
 import seaborn as sns
 import torch
-import tqdm
+from tqdm import tqdm
 
 from collections import defaultdict
 from typing import List
@@ -33,7 +33,9 @@ logger = logging.getLogger(__name__)
 
 ASCENDING_ORDER = {
     'variability': False,
+    'mean_variability': False,
     'confidence': True,
+    'final_confidence': True,
     'threshold_closeness': False,
     'forgetfulness': False,
     'correctness': True
@@ -83,7 +85,9 @@ def compute_train_dy_metrics(training_dynamics, args):
     - DataFrame with more typical training evaluation metrics, such as accuracy / loss.
     """
     confidence_ = {}
+    final_confidence_ = {}
     variability_ = {}
+    mean_variability_ = {}
     correctness_ = {}
     forgetfulness_ = {}
     threshold_closeness_ = {}
@@ -99,23 +103,28 @@ def compute_train_dy_metrics(training_dynamics, args):
 
     loss = torch.nn.CrossEntropyLoss()
 
-    num_tot_epochs = len(list(training_dynamics.values())[0]["logits"])
+    num_tot_epochs = np.max([len(record['logits']) for record in training_dynamics.values()])
     logger.info(f"Computing training dynamics across {num_tot_epochs} epochs")
-    logger.info(
-        "Metrics computed: confidence, variability, correctness, forgetfulness, threshold_closeness")
+    logger.info("Metrics computed: confidence, final confidence, variability, mean variability, correctness, forgetfulness, threshold_closeness")
 
     logits = {i: [] for i in range(num_tot_epochs)}
     targets = {i: [] for i in range(num_tot_epochs)}
     training_accuracy = defaultdict(float)
 
-    for guid in tqdm.tqdm(training_dynamics):
+    for guid in tqdm(training_dynamics):
         correctness_trend = []
         true_probs_trend = []
+        probs_per_label = [[],[],[]]
 
         record = training_dynamics[guid]
+        # skip examples that we do not have training dynamics for all epochs for
+        if len(record['logits']) < num_tot_epochs:
+            continue
         for i, epoch_logits in enumerate(record["logits"]):
-            probs = torch.nn.functional.softmax(
-                torch.Tensor(epoch_logits), dim=-1)
+            probs = torch.nn.functional.softmax(torch.Tensor(epoch_logits), dim=-1)
+            for l in range(3):
+                probs_per_label[l].append(float(probs[l]))
+            
             true_class_prob = float(probs[record["gold"]])
             true_probs_trend.append(true_class_prob)
 
@@ -129,8 +138,9 @@ def compute_train_dy_metrics(training_dynamics, args):
 
         correctness_[guid] = compute_correctness(correctness_trend)
         confidence_[guid] = np.mean(true_probs_trend)
+        final_confidence_[guid] = np.max([probs_per_label[l][-1] for l in range(3)])
         variability_[guid] = variability_func(true_probs_trend)
-
+        mean_variability_[guid] = np.mean([variability_func(probs_per_label[l]) for l in range(3)])
         forgetfulness_[guid] = compute_forgetfulness(correctness_trend)
         threshold_closeness_[guid] = threshold_closeness_func(confidence_[
                                                               guid])
@@ -142,14 +152,18 @@ def compute_train_dy_metrics(training_dynamics, args):
                     'index',
                     'threshold_closeness',
                     'confidence',
+                    'final_confidence',
                     'variability',
+                    'mean_variability',
                     'correctness',
-                    'forgetfulness', ]
+                    'forgetfulness']
     df = pd.DataFrame([[guid,
                         i,
                         threshold_closeness_[guid],
                         confidence_[guid],
+                        final_confidence_[guid],
                         variability_[guid],
+                        mean_variability_[guid],
                         correctness_[guid],
                         forgetfulness_[guid],
                         ] for i, guid in enumerate(correctness_)], columns=column_names)
@@ -161,6 +175,63 @@ def compute_train_dy_metrics(training_dynamics, args):
                               ] for i in range(num_tot_epochs)],
                             columns=['epoch', 'loss', 'train_acc'])
     return df, df_train
+
+
+def write_mixed_data(args, train_dy_metrics):
+    # First save the args for filtering, to keep track of which model was used for filtering.
+    argparse_dict = vars(args)
+    with open(os.path.join(args.output_dir, f"filtering_configs.json"), "w") as outfile:
+        outfile.write(json.dumps(
+            argparse_dict, indent=4, sort_keys=True) + "\n")
+    
+    is_ascending = ASCENDING_ORDER['variability']
+    if args.worst:
+        is_ascending = not is_ascending
+    sorted_ambi_scores = train_dy_metrics.sort_values(by=['variability'],
+                                                ascending=is_ascending)
+
+    is_ascending = not ASCENDING_ORDER['confidence']
+    sorted_easy_scores = train_dy_metrics.sort_values(by=['confidence'],
+                                                ascending=is_ascending)
+
+    original_train_file = os.path.join(args.data_dir, f"train.tsv")
+    train_numeric, header = read_data(original_train_file, task_name=args.task_name, guid_as_int=True)
+
+    # only one fraction
+    if not args.fraction:
+        args.fraction = 0.10
+    fractions_replace = [0.1, 0.2, 0.25, 0.33, 0.5]
+    for fraction_replace in fractions_replace:
+        outdir = os.path.join(args.output_dir,
+                            f"cartography_{args.metric}_{fraction_replace:.2f}/{args.task_name}")
+        if not os.path.exists(outdir):
+            os.makedirs(outdir)
+
+        # Dev and test need not be subsampled.
+        copy_dev_test(args.task_name,
+                      from_dir=args.data_dir,
+                      to_dir=outdir)
+
+        num_easy_samples = int(args.fraction * fraction_replace * len(train_numeric))
+        num_ambi_samples = int(args.fraction * len(train_numeric)) - num_easy_samples
+
+        with open(os.path.join(outdir, f"train.tsv"), "w") as outfile:
+            outfile.write(header + "\n")
+            selected_easy = sorted_easy_scores.head(n=num_easy_samples+1)
+            selected_ambiguous = sorted_ambi_scores.head(n=num_ambi_samples+1)
+            selected = pd.concat([selected_easy, selected_ambiguous])
+            selection_iterator = tqdm(range(len(selected)))
+            for idx in selection_iterator:
+                selection_iterator.set_description(f"mixed")
+                selected_id = selected.iloc[idx]["guid"]
+                if args.task_name in ["SNLI", "MNLI"]:
+                    selected_id = int(selected_id)
+                elif args.task_name == "WINOGRANDE":
+                    selected_id = str(int(selected_id))
+                record = train_numeric[selected_id]
+                outfile.write(record + "\n")
+
+        logger.info(f"Wrote {num_easy_samples + num_ambi_samples} samples to {outdir}.")
 
 
 def write_filtered_data(args, train_dy_metrics):
@@ -184,40 +255,42 @@ def write_filtered_data(args, train_dy_metrics):
         sorted_scores = train_dy_metrics.sort_values(by=[args.metric],
                                                     ascending=is_ascending)
 
-    original_train_file = os.path.join(os.path.join(args.data_dir, args.task_name), f"train.tsv")
+    original_train_file = os.path.join(args.data_dir, args.data_file)
     train_numeric, header = read_data(original_train_file, task_name=args.task_name, guid_as_int=True)
 
-    if args.fraction and 0 < args.fraction < 1:
-        fractions = [args.fraction]
+    if args.n and args.n > 1:
+        num_samples = [args.n]
+    elif args.fraction and 0 < args.fraction < 1:
+        num_samples = [int(args.fraction * len(train_numeric))]
     else:
         fractions = [0.01, 0.05, 0.10, 0.1667, 0.25, 0.33, 0.50, 0.75]
+        num_samples = [int(f * len(train_numeric)) for f in fractions]
 
-    for fraction in fractions:
-        outdir = os.path.join(args.output_dir,
-                              f"cartography_{args.metric}_{fraction:.2f}/{args.task_name}")
+    for n in num_samples:
+        outdir = args.output_dir
         if not os.path.exists(outdir):
             os.makedirs(outdir)
 
         # Dev and test need not be subsampled.
         copy_dev_test(args.task_name,
-                      from_dir=os.path.join(args.data_dir, args.task_name),
+                      from_dir=args.data_dir,
                       to_dir=outdir)
 
-        num_samples = int(fraction * len(train_numeric))
         with open(os.path.join(outdir, f"train.tsv"), "w") as outfile:
             outfile.write(header + "\n")
-            selected = sorted_scores.head(n=num_samples+1)
+            selected = sorted_scores.head(n=n)
             if args.both_ends:
-                hardest = sorted_scores.head(n=int(num_samples * 0.7))
-                easiest = sorted_scores.tail(n=num_samples - hardest.shape[0])
+                hardest = sorted_scores.head(n=int(n * 0.7))
+                easiest = sorted_scores.tail(n=n-hardest.shape[0])
                 selected = pd.concat([hardest, easiest])
                 fm = args.metric
                 logger.info(f"Selecting both ends: {fm} = "
                             f"({hardest.head(1)[fm].values[0]:3f}: {hardest.tail(1)[fm].values[0]:3f}) "
                             f"& ({easiest.head(1)[fm].values[0]:3f}: {easiest.tail(1)[fm].values[0]:3f})")
 
-            selection_iterator = tqdm.tqdm(range(len(selected)))
+            selection_iterator = tqdm(range(len(selected)))
             for idx in selection_iterator:
+                # set tqdm bar description
                 if args.metric == 'random':
                     selection_iterator.set_description('Random')
                 else:
@@ -232,7 +305,7 @@ def write_filtered_data(args, train_dy_metrics):
                 record = train_numeric[selected_id]
                 outfile.write(record + "\n")
 
-        logger.info(f"Wrote {num_samples} samples to {outdir}.")
+        logger.info(f"Wrote {n} samples to {outdir}.")
 
 
 def plot_data_map(dataframe: pd.DataFrame,
@@ -355,6 +428,10 @@ if __name__ == "__main__":
                         default="/Users/swabhas/data/glue/WINOGRANDE/xl/",
                         type=os.path.abspath,
                         help="Directory where data for task resides.")
+    parser.add_argument("--data_file",
+                        default='train.tsv',
+                        type=str,
+                        help="Name of data file.")
     parser.add_argument("--plots_dir",
                         default="./cartography/",
                         type=os.path.abspath,
@@ -371,15 +448,21 @@ if __name__ == "__main__":
     parser.add_argument('--metric',
                         choices=('threshold_closeness',
                                  'confidence',
+                                 'final_confidence',
                                  'variability',
+                                 'mean_variability',
                                  'correctness',
                                  'forgetfulness',
-                                 'random'),
+                                 'random',
+                                 'mixed'),
                         help="Metric to filter data by.",)
     parser.add_argument("--seed",
                         type=int,
                         default=725862,
                         help="Random seed for sampling.")
+    parser.add_argument('--n',
+                        type=int,
+                        help="Integer indicating number of random samples to select")
     parser.add_argument('--fraction',
                         type=float,
                         help="Number between 0 and 1, indicating fraction of random samples to select.")
@@ -401,22 +484,21 @@ if __name__ == "__main__":
     parser.add_argument("--overwrite_train_dy",
                         action="store_true",
                         help="Whether to overwrite previously computed training dynamics")
+    parser.add_argument("--split",
+                        type=str,
+                        default='training',
+                        help="Dataset split whose training dynamics to read")
 
     args = parser.parse_args()
-    train_dy_filename = os.path.join(args.model_dir, "td_metrics.jsonl")
+    train_dy_filename = os.path.join(args.model_dir, f"{args.split}_td_metrics.jsonl")
 
     if args.overwrite_train_dy or not os.path.exists(train_dy_filename):
-        training_dynamics = read_training_dynamics(args.model_dir)
+        training_dynamics = read_training_dynamics(args.model_dir, split=args.split)
         train_dy_metrics, _ = compute_train_dy_metrics(training_dynamics, args)
-
-        train_dy_filename = os.path.join(args.model_dir, "td_metrics.jsonl")
-        train_dy_metrics.to_json(train_dy_filename,
-                                orient='records',
-                                lines=True)
-        logger.info(
-            f"Metrics based on training dynamics written to {train_dy_filename}")
+        train_dy_metrics.to_json(train_dy_filename, orient='records', lines=True)
+        logger.info(f"Metrics for {args.split} data based on training dynamics written to {train_dy_filename}")
     else:
-        logger.info(f"Read metrics based on training dynamics from {train_dy_filename}")
+        logger.info(f"Read metrics for {args.split} data based on training dynamics from {train_dy_filename}")
         train_dy_metrics = pd.read_json(train_dy_filename, lines=True)
 
     if args.filter:
@@ -424,7 +506,12 @@ if __name__ == "__main__":
         if not os.path.exists(args.output_dir):
             os.makedirs(args.output_dir)
         assert args.metric
-        write_filtered_data(args, train_dy_metrics)
+
+        if args.metric == 'mixed':
+            write_mixed_data(args, train_dy_metrics)
+        else:
+            write_filtered_data(args, train_dy_metrics)
+
 
     if args.plot:
         assert args.plots_dir
