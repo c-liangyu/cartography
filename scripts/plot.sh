5148a732b6e1661e257155a6af854e62bc8f05d9
DavidChenL <3439388084@qq.com>
2022-01-22 12:53:14 +0800

    add dynamics logs

--- scripts/plot.sh
+++ scripts/plot.sh
@@ -1,9 +1,8 @@
-TASK=MNLI
-PATH_TO_MODEL_OUTPUT_DIR_WITH_TRAINING_DYNAMICS=output/mnli/
+TASK=PathMNIST
+PATH_TO_MODEL_OUTPUT_DIR_WITH_TRAINING_DYNAMICS=dynamics_logs/PATHMNIST
 
 python -m cartography.selection.train_dy_filtering \
     --plot \
     --task_name $TASK \
-    --plots_dir $PATH_TO_MODEL_OUTPUT_DIR_WITH_TRAINING_DYNAMICS \
-    --plot_title "MNLI Data Map" \
+    --plot_title "PathMNIST Data Map" \
     --model_dir $PATH_TO_MODEL_OUTPUT_DIR_WITH_TRAINING_DYNAMICS
